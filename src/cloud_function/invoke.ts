6d0fcf4302f4b9a65533e2001d9bb673ce2a9fef
Ben <ben@zfben.com>
2019-07-08 15:21:19 +0800

    add local invoke

--- src/cloud_function/invoke.ts
+++ src/cloud_function/invoke.ts
@@ -1,15 +1,25 @@
+import request from '@faasjs/request';
 import scf from './scf';
 import Tencentcloud from '..';
 
 export default function invokeCloudFunction (this: Tencentcloud, name: string, data: any, options?: any) {
   this.logger.debug('invokeFunction: %s %o', name, options);
 
-  return scf.call(this, Object.assign({
-    Action: 'Invoke',
-    FunctionName: name.replace(/[^a-zA-Z0-9-_]/g, '_'),
-    ClientContext: JSON.stringify(data),
-    InvocationType: 'Event',
-    Namespace: process.env.FaasEnv,
-    Qualifier: process.env.FaasEnv
-  }, options || {}));
+  if (process.env.FaasMode === 'local' && process.env.FaasLocal) {
+    return request(process.env.FaasLocal + '/' + name, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  } else {
+    return scf.call(this, Object.assign({
+      Action: 'Invoke',
+      FunctionName: name.replace(/[^a-zA-Z0-9-_]/g, '_'),
+      ClientContext: JSON.stringify(data),
+      InvocationType: 'Event',
+      Namespace: process.env.FaasEnv,
+      Qualifier: process.env.FaasEnv
+    }, options || {}));
+  }
 }
